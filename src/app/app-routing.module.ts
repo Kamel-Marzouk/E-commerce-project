cc3c8b77767b5608294f4ea46d1874b10133c65f
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-03-11 14:06:51 +0200

    Add Routing and add some components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SellerAuthComponent } from './components/seller-auth/seller-auth.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'seller-auth',
+    component:SellerAuthComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'cart',
+    component:CartComponent
+  },
+  {
+    path:'**',
+    component:PagenotfoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SellerAuthComponent } from './components/seller-auth/seller-auth.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SellerAuthComponent,
+    PagenotfoundComponent,
+    LoginComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
