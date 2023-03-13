62a2c23f24720ecafcffb4d7e688f088cde20c73
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-03-13 09:16:02 +0200

    add login form

--- src/app/components/seller-auth/seller-auth.component.ts
+++ src/app/components/seller-auth/seller-auth.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Seller } from 'src/app/models/seller';
 import { SellerService } from 'src/app/services/seller.service';
@@ -8,14 +8,28 @@ import { SellerService } from 'src/app/services/seller.service';
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css'],
 })
-export class SellerAuthComponent implements OnInit{
+export class SellerAuthComponent implements OnInit {
+  showLogin: boolean = false;
+
   constructor(private sellerService: SellerService, private router: Router) {}
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.sellerService.reloadSeller();
   }
 
   signUp(data: Seller): void {
     this.sellerService.addSeller(data);
+  }
+
+  login(data: Seller): void {
+    console.log(data);
+  }
+
+  openLogin(): void {
+    this.showLogin = true;
+  }
+
+  openSignUp(): void {
+    this.showLogin = false;
   }
 }
