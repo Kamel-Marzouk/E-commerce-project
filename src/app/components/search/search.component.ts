4e1a11ed2d7404ba701d8aabf19d19dfd6a772af
Kamel-Marzouk <k.marzouk.amin@gmail.com>
2023-03-22 11:12:35 +0200

    handle Search product from API

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchResult: undefined | Product[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.getSearchResult();
+  }
+
+  getSearchResult(): void {
+    const query: null | string = this.route.snapshot.paramMap.get('query');
+    if (query) this.productService.searchProducts(query) .subscribe((result: Product[]) => this.searchResult = result);
+  }
 
 }
